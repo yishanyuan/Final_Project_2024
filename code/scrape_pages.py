26b6fc8a6098e31284981765fa09a7146cd4c02b
王毓晨 <wangyuchen@wireless-10-148-116-11.public.utexas.edu>
2024-11-20 19:59:05 -0600

    get soup and scrape links

---
+++ code/scrape_pages.py
@@ -0,0 +1,37 @@
+
+from common import get_soup
+URL = "https://guide.michelin.com/us/en/restaurants/all-starred/bib-gourmand/page/"
+
+
+def scrape_page(num):
+    """Takes a page and returns a list of links to the book that are on the page."""
+    url = URL + str(num)
+    res_links = get_soup(url).find_all("h3", class_ = "card__menu-content--title pl-text pl-big js-match-height-title")
+    links = []
+    for res in res_links:
+        for a_tag in res.find_all("a", href = True):
+            href = a_tag.get("href")
+            full_link = "https://guide.michelin.com/" + str(href)
+            links.append(full_link)
+
+    return links
+
+def scrape_all_pages():
+    """Scrapes all pages, returning a list of book links."""
+    page = 1
+
+    links = []
+
+    while True:
+
+        page_data = scrape_page(page)
+
+        if not page_data:
+            break
+
+        page += 1
+        links += page_data
+
+    return links
+    
+
