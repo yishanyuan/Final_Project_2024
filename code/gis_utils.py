a863fc31c8cef937635e585ed9f60b762721dd39
yishanyuan <yishanyuan@utexas.edu>
2024-12-06 17:28:57 -0600

    Update gis_utils.py

--- code/gis_utils.py
+++ code/gis_utils.py
@@ -1,51 +1,55 @@
+import os
 import pandas as pd
 import geopandas as gpd
 from sqlalchemy import create_engine
 import ast
+from dotenv import load_dotenv
+from shapely.geometry import Point
 
-# PostgreSQL connection parameters
-db_params = {
-    "dbname": "finalproject2024",
-    "user": "postgres",
-    "password": "pumpkinpie",
-    "host": "34.57.167.81",
-    "port": "5432"
-}
 
-# Function to create connection to PostgreSQL
+# 加载环境变量
+load_dotenv()
+
+DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
+DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
+DATABASE_HOST = os.environ["DATABASE_HOST"]
+DATABASE_PORT = os.environ["DATABASE_PORT"]
+DATABASE_DATABASE = os.environ["DATABASE_DATABASE"]
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DATABASE}"
+
+# 创建数据库连接
 def get_database_connection():
-    engine = create_engine(f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}")
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
     return engine
 
-# Function to query data from the database
+# 查询数据并返回 GeoDataFrame
 def query_data():
     engine = get_database_connection()
     query = """
-    SELECT name, latitude_and_longitude
+    SELECT name, food_type, address, stars_label, latitude_and_longitude
     FROM cleaned_data_with_embeddings
-    WHERE latitude_and_longitude IS NOT NULL; 
+    WHERE latitude_and_longitude IS NOT NULL;
     """
     df = pd.read_sql(query, engine)
 
-    # Extract latitude and longitude from the 'latitude and longitude' column
+    # 提取经纬度
     def extract_lat_lng(value):
         try:
-            # Assuming 'latitude and longitude' is stored as a string dictionary
             value_dict = ast.literal_eval(value)
             return value_dict.get('lat'), value_dict.get('lng')
         except (ValueError, SyntaxError, TypeError):
             return None, None
 
     df['latitude'], df['longitude'] = zip(*df['latitude_and_longitude'].apply(extract_lat_lng))
-
-    # Drop rows with missing latitude or longitude
     df = df.dropna(subset=['latitude', 'longitude'])
 
-    # Convert DataFrame to GeoDataFrame
+    # 转换为 GeoDataFrame
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
     return gdf
 
-# Example usage
-if __name__ == "__main__":
-    gdf = query_data()
-    print(gdf.head())
+# 计算最近的餐厅
+def get_nearest_restaurants(selected_point, gdf, num_results=10):
+    gdf['distance'] = gdf.geometry.apply(lambda x: selected_point.distance(x))
+    nearest_restaurants = gdf.sort_values('distance').head(num_results)
+    return nearest_restaurants
