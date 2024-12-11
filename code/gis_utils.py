8edc769055779f00f0f3264267de2a5f0e262a64
yishanyuan <yishanyuan@utexas.edu>
2024-12-10 19:48:37 -0600

    Update gis_utils.py

--- code/gis_utils.py
+++ code/gis_utils.py
@@ -3,7 +3,6 @@ import geopandas as gpd
 import ast
 from code.database import get_engine
 
-# 查询数据并返回 GeoDataFrame
 def query_data():
     query = """
     SELECT name, food_type, address, stars_label, latitude_and_longitude
@@ -14,7 +13,6 @@ def query_data():
     with engine.connect() as connection:
         df = pd.read_sql(query, connection)
 
-    # 提取经纬度
     def extract_lat_lng(value):
         try:
             value_dict = ast.literal_eval(value)
@@ -25,11 +23,9 @@ def query_data():
     df['latitude'], df['longitude'] = zip(*df['latitude_and_longitude'].apply(extract_lat_lng))
     df = df.dropna(subset=['latitude', 'longitude'])
 
-    # 转换为 GeoDataFrame
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
     return gdf
 
-# 计算最近的餐厅
 def get_nearest_restaurants(selected_point, gdf, num_results=10):
     gdf['distance'] = gdf.geometry.apply(lambda x: selected_point.distance(x))
     nearest_restaurants = gdf.sort_values('distance').head(num_results)
